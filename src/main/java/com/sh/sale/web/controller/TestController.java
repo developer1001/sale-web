dd4492e25524792c27024d6e5d34e1a41b381cf3
developer1001 <yangpeng_188@sina.com>
2018-12-19 10:41:50 +0800

    Initial commit

---
+++ src/main/java/com/sh/sale/web/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.sh.sale.web.controller;
+
+import com.sh.sale.common.Person;
+
+public class TestController {
+
+	
+	public static void main(String[] args) {
+		Person p = new Person();
+		System.out.println(p.getName() +"***" +p.getAge());
+	}
+	
+}
